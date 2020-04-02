1b47f48b460e3e2deeca585c8b2fe4a337de4712
souravbanerjee23 <62287294+souravbanerjee23@users.noreply.github.com>
2020-04-02 13:49:11 +0530

    Add files via upload

---
+++ HolidayParty-Validations/HolidayParty_new/src/main/java/com/controller/RegistrationController.java
@@ -0,0 +1,13 @@
+package com.controller;
+import com.model.*;
+import org.springframework.validation.BindingResult;
+public class RegistrationController {
+
+	  public String performRegistration( RegistrationBean  registrationBean, 
+			BindingResult result) {
+		
+            	return null;
+	
+	
+	}	 	  		    	    	     	      	 	
+}
---
+++ HolidayParty-Validations/HolidayParty_new/src/main/java/com/validate/CustomValidator.java
@@ -0,0 +1,21 @@
+package com.validate;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+public class CustomValidator implements Validator{
+
+	
+	public void validate(Object arg0, Errors arg1) {
+		
+		
+		
+
+	
+	}	 	  	    	    	     	      	 	
+
+	public boolean supports(Class<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+}
